4e302470a608cd6b373d3774ca200f96a48f2002
Kervion <kiu@kervion.com>
2023-10-16 14:48:21 +0200

    version not working correctly

--- App.js
+++ App.js
@@ -1,91 +1,70 @@
-import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
-// import axios from "axios"
+import { StyleSheet, View, Text } from "react-native"
 import { useEffect } from "react"
 import SNSMobileSDK from "@sumsub/react-native-mobilesdk-module"
 
-// console.log(Object.keys(SNSMobileSDK))
-// SNSMobileSDK.init("token", () => {
-//   console.log("kuku 1")
-//   // build()
-// })
+const SERVER = "http://192.168.0.171:3000/sumsub-access-token"
+const TOKEN = "_act-sbx-9aa33b74-42c7-4220-9af5-55352f85aff5"
 
 export default function App() {
+  useEffect(() => {
+    // launchSNSMobileSDK(TOKEN)
+    fetchToken()
+  }, [])
+
   const fetchToken = async () => {
     try {
-      const response = await fetch("http://192.168.0.171:3000/sumsub-access-token")
+      const response = await fetch(SERVER)
       if (!response.ok) {
         throw new Error("Network response was not ok")
       }
       const data = await response.json()
-      return data.token.token
-      // console.log(data.token.token)
+      // THE ANSWER MAY BE SLIGHTLY DIFFERENT ON YOUR SERVER
+      console.log("TOKEN : ", data.token.token)
+      launchSNSMobileSDK(data.token.token)
     } catch (error) {
       console.error("Error fetching data: ", error)
     }
   }
 
-  function listAllMethods(obj) {
-    let methods = new Set()
-    while (obj) {
-      Object.getOwnPropertyNames(obj).forEach((property) => {
-        if (typeof obj[property] === "function" && property !== "constructor") {
-          methods.add(property)
-        }
+  const launchSNSMobileSDK = (acct) => {
+    const snsMobileSDK = SNSMobileSDK.init(acct, () => {
+      return fetch(SERVER, {
+        method: "GET",
+      }).then((resp) => {
+        const data = resp.json()
+        return data.token.token
       })
-      obj = Object.getPrototypeOf(obj)
-    }
-    return Array.from(methods)
+    })
+      // THOSE ARE ONLY OPTIONAL CONFIGURATIONS
+      .withHandlers({
+        onStatusChanged: (event) => {
+          console.log("onStatusChanged: [" + event.prevStatus + "] => [" + event.newStatus + "]")
+        },
+        onLog: (event) => {
+          console.log("onLog: [Idensic] " + event.message)
+        },
+      })
+      .withDebug(true)
+      .withLocale("en")
+      // END OF OPTIONAL CONFIGURATIONS
+      .build()
+
+    console.log("builded")
+    // UNTIL HERE EVERYTHING IS OK, THEN LAUNCH NULL IS RETURNED
+
+    snsMobileSDK
+      .launch()
+      .then((result) => {
+        console.log("SumSub SDK State: " + JSON.stringify(result))
+      })
+      .catch((err) => {
+        console.log("SumSub SDK Error: " + JSON.stringify(err))
+      })
   }
-
-  let launchSNSMobileSDK = async () => {
-    let accessToken = "123"
-    // let accessToken = await fetchToken()
-    // console.log("accessToken: " + accessToken)
-    let snsMobile = SNSMobileSDK.init(accessToken, () => {})
-    const build = await snsMobile.build()
-
-    console.log(Object.keys(build))
-    console.log(Object.keys(build.sdkConf))
-    console.log(listAllMethods(snsMobile))
-
-    // let wynik = await snsMobile?.launch()
-
-    // snsMobile.build().then(() => {
-    //   console.log(listAllMethods(snsMobile))
-    // })
-
-    // console.log("kukux")
-    // console.log(Object.keys(snsMobile))
-
-    // console.log(snsMobile.debug)
-    // console.log(snsMobile.isAnalyticsEnabled)
-    // console.log(snsMobile.handlers)
-    // console.log(snsMobile.applicantConf)
-    // console.log(snsMobile.preferredDocumentDefinitions)
-    // console.log(snsMobile.autoCloseOnApprove)
-    // console.log(snsMobile.settings)
-    // console.log(snsMobile.accessToken)
-    // console.log(snsMobile.tokenExpirationHandler)
-
-    // snsMobile
-    //   .launch()
-    //   .then((result) => {
-    //     console.log("SumSub SDK State: " + JSON.stringify(result))
-    //   })
-    //   .catch((err) => {
-    //     console.log("SumSub SDK Error: " + JSON.stringify(err))
-    //   })
-  }
-
-  useEffect(() => {
-    launchSNSMobileSDK()
-  }, [])
 
   return (
     <View style={styles.container}>
       <Text>Checking Sumsub Library</Text>
-      {/* <StatusBar style="auto" /> */}
     </View>
   )
 }
